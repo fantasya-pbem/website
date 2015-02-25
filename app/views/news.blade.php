71ea46056408e754d22cfa7e37e14d1073e300b9
root <root@v22014081585820161.yourvserver.net>
2015-02-25 16:16:18 +0100

    Befehlsadresse ersetzt.

--- app/views/news.blade.php
+++ app/views/news.blade.php
@@ -7,7 +7,7 @@
 @section('text')
 	<p>Seit Anfang 2015 ist Fantasya wieder online!</p>
 	<p>Zur Zeit sind wir noch mit dem Wiederaufbau beschäftigt, daher funktioniert die Zugabgabe noch nicht so, wie ihr es von früher gewohnt seid. Wir halten Euch in der <em>Taverne</em>, unserem Forum, auf dem Laufenden, und senden E-Mail-Nachrichten an die alten Spieleradressen.</p>
-	<p>Bitte richtet Eure Anfragen und Spielbefehle zunächst an die E-Mail-Adresse <strong>fantasya-host(at)web.de</strong>, oder fragt einfach in der Taverne nach.</p>
+	<p>Spielbefehle bitte an die E-Mail-Adresse <strong>befehle(at)fantasya-pbem.de</strong> senden. Für Fragen ist unser Forum, die Taverne, der geeignete Ort.</p>
 	
 	@foreach ($news as $article)
 	    <div class="article">
