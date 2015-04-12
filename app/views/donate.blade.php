cff1d55d15fac5cf23aa51d2dc5fefa9468c8873
Sascha Ternes <thalian@online.de>
2015-04-12 22:48:03 +0200

    Neue Bitcoin-Adresse, QR-Code auf Spendenseite ergänzt.

--- app/views/donate.blade.php
+++ app/views/donate.blade.php
@@ -8,6 +8,7 @@
 	<p>Der Betrieb des Servers für Fantasya kostet Geld.</p>
 	<p>Es ist nicht viel, aber vielleicht gefällt Dir Fantasya so gut, dass Du gerne einen kleinen Beitrag zur Finanzierung leisten möchtest.</p>
 	<p>Vielleicht kennst Du Bitcoins, die mittlerweile weit verbreitete virtuelle Währung des Internets, und möchtest ein paar mBTC für Fantasya spenden? Dann sende Deine Spende an diese Adresse:</p>
-	<p><code>13M8UCJzDb1gQtwCrTMYRadseSHUSy1Z7n</code></p>
+	<p><code>18GPpNuzLGJze9Hep8do8haTDfadjY3wtu</code></p>
 	<p>Dankeschön!</p>
+	<p style="text-align: center"><img src="/images/donate.png" />
 @stop
