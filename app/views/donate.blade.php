a0351cdfca9e9ffe2285eae03255a806ff318919
Sascha Ternes <thalian@online.de>
2015-04-13 07:54:08 +0200

    QR-Code verlinkt.

--- app/views/donate.blade.php
+++ app/views/donate.blade.php
@@ -10,7 +10,7 @@
 	<p>Vielleicht kennst Du Bitcoins, die mittlerweile weit verbreitete virtuelle Währung des Internets, und möchtest ein paar mBTC für Fantasya spenden? Dann sende Deine Spende an diese Adresse:</p>
 	<p><code>18GPpNuzLGJze9Hep8do8haTDfadjY3wtu</code></p>
 	<p>Dankeschön!</p>
-	<p style="text-align: center"><img src="/images/donate.png" />
+	<p style="text-align: center"><a href="bitcoin:18GPpNuzLGJze9Hep8do8haTDfadjY3wtu"><img src="/images/donate.png" /></a>
 	@if (Auth::user() && Auth::user()->name === 'Thalian')
 		<p><a href="https://blockchain.info/address/18GPpNuzLGJze9Hep8do8haTDfadjY3wtu" target="_blank">Spendenstatus in der Blockchain überprüfen</a></p>
 	@endif
