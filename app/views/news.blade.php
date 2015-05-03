b128ef37dfd9440866972cf09a02896887accda7
Sascha Ternes <thalian@online.de>
2015-05-03 15:20:44 +0200

    Neuigkeiten angepasst.

--- app/views/news.blade.php
+++ app/views/news.blade.php
@@ -6,7 +6,6 @@
 
 @section('text')
 	<p>Seit Anfang 2015 ist Fantasya wieder online!</p>
-	<p>Der Wiederaufbau ist mittlerweile schon recht weit vorangeschritten. Was noch fehlt ist die Registrierung für neue Spieler und einige Komfortfunktionen, aber wir sind da dran! Wir halten Euch in der <em>Taverne</em>, unserem Forum, auf dem Laufenden, und senden E-Mail-Nachrichten an die alten Spieleradressen.</p>
 	<p>Spielbefehle bitte an die E-Mail-Adresse <strong>befehle(at)fantasya-pbem.de</strong> senden. Für Fragen ist unser Forum, die Taverne, der geeignete Ort.</p>
 	
 	@foreach ($news as $article)
