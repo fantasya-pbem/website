f4d297c0dc4c964c85e7be7533b5e736491d35af
Sascha Ternes <thalian@online.de>
2016-04-08 18:40:28 +0200

    Fehler bei Gesamtbevölkerung behoben.

--- app/controllers/FantasyaController.php
+++ app/controllers/FantasyaController.php
@@ -312,7 +312,7 @@ class FantasyaController extends BaseController {
 		$underworld = DB::connection($game->database)->table('regionen')->select(DB::raw('typ, COUNT(*) AS count'))->where('welt', -1)->groupBy('typ')->orderBy('typ')->get();
 		$units      = DB::connection($game->database)->table('einheiten')->select(DB::raw('rasse, COUNT(*) AS units, SUM(person) AS persons'))->whereNotIn('partei', [620480, 1376883])->groupBy('rasse')->orderBy('rasse')->get();
 		$monsters   = DB::connection($game->database)->table('einheiten')->select(DB::raw('rasse, COUNT(*) AS units, SUM(person) AS persons'))->whereIn('partei', [620480, 1376883])->groupBy('rasse')->orderBy('rasse')->get();
-		$total      = DB::connection($game->database)->table('einheiten')->select(DB::raw('COUNT(*) AS units, SUM(person) AS persons'))->groupBy('rasse')->get();
+		$total      = DB::connection($game->database)->table('einheiten')->select(DB::raw('COUNT(*) AS units, SUM(person) AS persons'))->get();
 		return View::make('world', array('game' => $game, 'turn' => $turn->Value, 'lastZAT' => $lastZAT, 'count' => $count, 'parties' => $parties, 'names' => $names, 'regions' => $regions, 'underworld' => $underworld, 'layers' => $layers, 'units' => $units, 'monsters' => $monsters, 'total' => $total));
 	}
 
