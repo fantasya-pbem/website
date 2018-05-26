e220a939e7ed988d52dcb2accaf4da157dabcfee
pcbaer <pcbaer@online.de>
2018-05-26 17:53:52 +0200

    Github-Repository unter Entwicklung verlinkt.

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -21,7 +21,8 @@
 					<li><a href="/index">Fantasya</a></li>
 					<li><a href="//wiki.fantasya-pbem.de">Bibliothek</a></li>
 					<li><a href="//forum.fantasya-pbem.de">Taverne</a></li>
-					<li class="last"><a href="http://webchat.freenode.net/?channels=%23fantasya&uio=OT10cnVlJjExPTQx30" target="fantasya_chat">Chat</a></li>
+					<li><a href="http://webchat.freenode.net/?channels=%23fantasya&uio=OT10cnVlJjExPTQx30" target="fantasya_chat">Chat</a></li>
+					<li class="last"><a href="https://github.com/fantasya-pbem/host" target="fantasya_entwicklung">Entwicklung</a></li>
 				</ul>
 			</div>
 
