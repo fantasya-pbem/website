4049ac65d9cdc15a43ac02bbeb297f0c1d45bcf8
pcbaer <pcbaer@online.de>
2018-06-24 13:07:54 +0200

    Liberapay integriert

--- app/views/donate.blade.php
+++ app/views/donate.blade.php
@@ -7,6 +7,10 @@
 @section('text')
 	<p>Der Betrieb des Servers für Fantasya kostet Geld.</p>
 	<p>Es ist nicht viel, aber vielleicht gefällt Dir Fantasya so gut, dass Du gerne einen kleinen Beitrag zur Finanzierung leisten möchtest.</p>
+	<p>
+		<script src="https://liberapay.com/Fantasya/widgets/button.js"></script>
+		<noscript><a href="https://liberapay.com/Fantasya"><img alt="Spende über Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"></a></noscript>
+	</p>
 	<p>Vielleicht kennst Du Bitcoins, die mittlerweile weit verbreitete virtuelle Währung des Internets, und möchtest ein paar mBTC für Fantasya spenden? Dann sende Deine Spende an diese Adresse:</p>
 	<p><code>18GPpNuzLGJze9Hep8do8haTDfadjY3wtu</code></p>
 	<p>Dankeschön!</p>
