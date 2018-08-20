195fdca0e5b844e8b8bb8782f516b15a63767735
pcbaer <pcbaer@online.de>
2018-08-20 12:00:24 +0200

    Korrektur.

--- app/views/register-form.blade.php
+++ app/views/register-form.blade.php
@@ -8,7 +8,7 @@
 		<?php echo Form::label('captcha', 'Anti-Spam: ' . $captcha); ?><br>
 		<?php echo Form::text('captcha'); ?><span class="error"><?php echo $errors->first('captcha'); ?></span><br>
 		<br>
-		<p>Die Antwort auf die Anti-Spam-Frage findest Du in der Bibliothek.</p>
+		<p>Die Antwort auf die Anti-Spam-Frage findest Du in der Taverne.</p>
 		<?php echo Form::submit('Registrieren'); ?><br>
 	{{Form::close()}}
 </div>
