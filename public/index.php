0073ee7956b9bf58caf1eb4c5eec3acd4fb8898f
Thalian <github@fantasya-pbem.de>
2019-03-09 11:42:22 +0100

    [FEATURE] #18 – Migration zu Symfony: Fehlerseite erstellt, aufgeräumt.

--- public/index.php
+++ public/index.php
@@ -10,14 +10,11 @@ require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
-
     Debug::enable();
 }
-
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
-
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
