9583208b2c566ea5db633789f7da921228fd6c3a
Thalian <github@fantasya-pbem.de>
2019-08-24 20:10:11 +0200

    [FEATURE] #37 – Option für Versand mit Auswertung einbauen
    Use mail attachment flag in report mail sending.

--- bin/game/run.sh
+++ bin/game/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 HOST=localhost
-DATABASE=
-USER=
-PASSWORD=''
+DATABASE_GAME=
+USER_GAME=
+PASSWORD_GAME=''
+DATABASE_USER=
+USER_USER=
+PASSWORD_USER=''
 BACKUP=1
 TRUNCATE_MESSAGES=0
-LAST_TURN=`mysql -N -h $HOST -D $DATABASE -u $USER -p$PASSWORD -e "SELECT Value FROM settings WHERE Name='game.runde'"`
+LAST_TURN=`mysql -N -h $HOST -D $DATABASE_GAME -u $USER_GAME -p$PASSWORD_GAME -e "SELECT Value FROM settings WHERE Name='game.runde'"`
 TURN=`expr $LAST_TURN + 1`
 GAME=spiel
 GAME_ID=1
@@ -41,7 +44,7 @@ echo "Running turn $TURN..." >> $LOG
 if [ "$BACKUP" -gt 0 ]
 then
 	echo "Creating MySQL dump..." >> $LOG
-	mysqldump -h $HOST -u $USER -p$PASSWORD $DATABASE | gzip -c --best > $BACKUP_DIR/before-$TURN.sql.gz 2>> $LOG
+	mysqldump -h $HOST -u $USER_GAME -p$PASSWORD_GAME $DATABASE_GAME | gzip -c --best > $BACKUP_DIR/before-$TURN.sql.gz 2>> $LOG
 fi
 if [ "$TRUNCATE_MESSAGES" -gt 0 ]
 then
@@ -56,13 +59,13 @@ then
 			monsterParties=$monsterParties,$number
 		fi
 	done
-	mysql -h $HOST -D $DATABASE -u $USER -p$PASSWORD -e "DELETE FROM meldungen WHERE kategorie = 'Battle' AND partei NOT IN ($monsterParties)" 2>&1 >> $LOG
+	mysql -h $HOST -D $DATABASE_GAME -u $USER_GAME -p$PASSWORD_GAME -e "DELETE FROM meldungen WHERE kategorie = 'Battle' AND partei NOT IN ($monsterParties)" 2>&1 >> $LOG
 fi
 echo >> $LOG
 
 echo "Running the game..." >> $LOG
 TIMER_START=`date +%s`
-java -jar fantasya.jar -server $HOST:3306 -datenbank $DATABASE -benutzer $USER -passwort $PASSWORD -zat 2>&1 > $ZAT_LOG
+java -jar fantasya.jar -server $HOST:3306 -datenbank $DATABASE_GAME -benutzer $USER_GAME -passwort $PASSWORD_GAME -zat 2>&1 > $ZAT_LOG
 ZAT_RESULT=$?
 echo "Fantasya exit code: $ZAT_RESULT" >> $LOG
 TIMER_END=`date +%s`
@@ -96,16 +99,24 @@ do
 		monsterParties="$monsterParties,'$id'"
 	fi
 done
-for ID in `mysql -N -s -h $HOST -u $USER -D $DATABASE -p$PASSWORD -e "SELECT id FROM partei WHERE id NOT IN ($monsterParties)"`
+for ID in `mysql -N -s -h $HOST -u $USER_GAME -D $DATABASE_GAME -p$PASSWORD_GAME -e "SELECT id FROM partei WHERE id NOT IN ($monsterParties)"`
 do
-	EMAIL=`mysql -N -s -h $HOST -u $USER -D $DATABASE -p$PASSWORD -e "SELECT email FROM partei WHERE id = '$ID'"`
+	EMAIL=`mysql -N -s -h $HOST -u $USER_GAME -D $DATABASE_GAME -p$PASSWORD_GAME -e "SELECT email FROM partei WHERE id = '$ID'"`
+	WITH_ATTACHMENT=`mysql -N -s -h $HOST -u $USER_USER -D $DATABASE_USER -p$PASSWORD_USER -e "SELECT flags & 1 FROM user WHERE email = '$EMAIL'"`
 	EMAIL_TOKEN=`$FANTASYACOMMAND download:token $GAME_ID $ID $EMAIL $TURN`
 	if [ $? -eq 0 ]
 	then
 		cat $EMAIL_TEMPLATE > $EMAIL_TEXT
 		echo "$EMAIL_LINK/$EMAIL_TOKEN" >> $EMAIL_TEXT
 		echo "$ID -> $EMAIL" >> $LOG
-		mutt -F $EMAIL_DIR/muttrc -s "$EMAIL_SUBJECT" -- $EMAIL < $EMAIL_TEXT 2>&1 >> $LOG
+		if [ $WITH_ATTACHMENT -eq 1 ]
+		then
+			ZIP=$ZIP_DIR/$TURN/$TURN-$ID.zip
+			echo "$ZIP -> $EMAIL" >> $LOG
+			mutt -F $EMAIL_DIR/muttrc -s "$EMAIL_SUBJECT" -a $ZIP -- $EMAIL < $EMAIL_TEXT 2>&1 >> $LOG
+		else
+			mutt -F $EMAIL_DIR/muttrc -s "$EMAIL_SUBJECT" -- $EMAIL < $EMAIL_TEXT 2>&1 >> $LOG
+		fi
 		echo $(cat $EMAIL_TEXT) > $EMAIL_LOG/$EMAIL.mail 2>> $LOG
 	else
 		echo "Creation of download token failed for $EMAIL! No mail sent." >> $LOG
